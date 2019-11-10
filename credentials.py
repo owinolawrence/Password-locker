15950114035d722dca2bc7cd12c6aa2045d0ca48
owinolawrence <owinolawrence21@gmail.com>
2019-11-11 00:36:41 +0300

    create class create_user that create new user

--- credentials.py
+++ credentials.py
@@ -1,2 +1,9 @@
 from user import user
 from user import Credential
+
+def create_user(username, password):
+    """
+    function that create enable a person to create a new user
+    """
+    new_user(username,password)
+    return new_user
