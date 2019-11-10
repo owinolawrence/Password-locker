e7596d5c85663c9cffe077a2523794b75dd733db
owinolawrence <owinolawrence21@gmail.com>
2019-11-10 08:58:20 +0300

    import unittest and create TestUsers subclass

---
+++ user_test.py
@@ -0,0 +1,11 @@
+import unittest #Importing the unittest module
+from user import User # Importing class user
+
+class TestUser(unittest.TestCase):
+    """
+    Test class that defines test cases for the user class behaviours.
+    
+    Args:
+         unittest.TestCase:TestCase that help in creating test cases
+    """
+         
