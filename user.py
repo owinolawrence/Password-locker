2ef03d84450840da1638ff46b520f78a00d3378d
owinolawrence <owinolawrence21@gmail.com>
2019-11-10 08:42:57 +0300

    fix identation error

--- user.py
+++ user.py
@@ -2,12 +2,10 @@ class User:
     """
     Class that generate new instance of us user.
     """
-  user_list = [] # Empty user list
-  def___init___(self,login_username,user_password):
-    """
-    __init__ method helps us define properties for our objects.
-     
-    """
-    self.login_username = login_username
-    self.user_password = user_password
+    user_list= [] 
+  
+    def ___init___(self,login_username,user_password) :
     
+   
+        self.login_username = login_username
+        self.user_password = user_password
