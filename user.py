d1db33d8ad8f2bbc747ad4f9765831ab74c97a57
owinolawrence <owinolawrence21@gmail.com>
2019-11-10 08:20:05 +0300

    create two instance variable

--- user.py
+++ user.py
@@ -2,7 +2,11 @@ class User:
     """
     Class that generate new instance of us user.
     """
-  def___init___(self,login_username,user_password)
+  def___init___(self,login_username,user_password):
     """
-    
+    __init__ method helps us define properties for our objects.
+     
+    """
+    self.login_username = login_username
+    self.user_password = user_password
     
