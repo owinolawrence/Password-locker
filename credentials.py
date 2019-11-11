a03c72fdbf37f4380070a538471dfd47828dd265
owinolawrence <owinolawrence21@gmail.com>
2019-11-11 11:15:34 +0300

    create  a find_credential function

--- credentials.py
+++ credentials.py
@@ -1,9 +1,27 @@
-from user import user
+from user import User
 from user import Credential
-
+import random
 def create_user(username, password):
     """
     function that create enable a person to create a new user
     """
-    new_user(username,password)
+    new_user = User(username,password)
     return new_user
+
+def save_user(user):
+    """
+function that save new created user
+    """
+    user.save_user()
+
+def find_credential(account_name):
+    '''
+    Function that finds credentials by account name
+    '''
+
+    return Credential.find_by_name(account_name)
+
+
+ 
+if __name__ == '__main__':
+    main()
