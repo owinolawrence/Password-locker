54f8c627740edf125739bf79942ac0b047d98dc9
owinolawrence <owinolawrence21@gmail.com>
2019-11-11 23:57:48 +0300

    change credentials.py to run.py

--- run.py
+++ run.py
@@ -1,37 +1,66 @@
+import random
 from user import User
 from user import Credential
-import random
+
+
+
 def create_user(username, password):
     """
     function that create enable a person to create a new user
     """
-    new_user = User(username,password)
+    new_user = User()
     return new_user
+def create_credential(username, account_name, account_password):
+    """
+    function to enable a person to create a new credential
+    """
+    new_credential = Credential (username, account_name, account_password)
+    return new_credential
 
 def save_user(user):
     """
-function that save new created user
+    function that save new created user
     """
     user.save_user()
+def save_credential(Credential):
+    """
+    function that save credential
+    """
+    Credential.save_credential()
+
+
+def check_existing_user(name):
+    '''
+    check if user exists
+    '''
+    return User.user_exists(name)
+
 
 def find_credential(account_name):
     '''
     Function that finds credentials by account name
     '''
-
+    # new_credential = Credential(acc, name, password)
     return Credential.find_by_name(account_name)
+
+
 def existing_credentials(name):
     '''
     Functiion that checks if an account really exists
     '''
-    return Credential.credential_exist(name)
+    Credential.credential_exists(name)
 
-
+def user_exist (name):
+    """
+    Functiion that checks if an account really exists
+    """
+    User.user_exists(name)
+    return name
 def delete_credential(credentials):
     '''
     Function that deletes credentials that are no longer in use
     '''
-    return Credential.delete_credential(credentials)
+    return Credential.delete_credentials(credentials)
 
 
 def display_credential():
@@ -39,14 +68,16 @@ def display_credential():
     Functiomn that displays all the saved credentials 
     '''
     return Credential.display_credentials()
+
+
 def main():
     while True:
         print("*** PASSWORD LOCKER ***")
         print('_'*30)
-        print("Do you have an account with password Locker? y/n")
+        print("Do you have an account with password Locker? yes/no")
         print("use short 'ex' to  log out")
         option = input().lower()
-        if option == "y":
+        if option == "yes":
 
             print('*** Login ***')
             print('Enter username \n')
@@ -54,19 +85,24 @@ def main():
             print('\n')
             print('Enter password')
             nompass = input()
- print(f"Welcome:{nomname} to your Account ")
-            print('*'*40)
+            authentication = user_exist(nomname)
 
-            print("Select an option either 1,2,3,4 or 5")
-            print('\n')
+            if authentication == True:
+                print(f"Welcome:{nomname} to your Account ")
+                print('*'*40)
 
-            while True:
+                print("Select an option either 1,2,3,4 or 5")
+                print('\n')
+
+                while True:
                     print('1:Add credential')
                     print('2:View saved credential')
                     print('3:Delete Credentials')
                     print('4:Search Credentials')
                     print('5:Leave')
-                    selected = input()if selected == '1':
+                    selected = input()
+
+                    if selected == '1':
                         while True:
                             print('Do you want to proceed? y/n')
 
@@ -90,17 +126,22 @@ def main():
                                     accpass = input()
                                     print(
                                         f"Account:{accname} Password:{accpass}")
-                                    elif decision == 'n':
+
+                                else:
+                                    print("Invalid choice")
+                                save_credential(
+                                    create_credential(accname, uname, accpass))
+                            elif decision == 'n':
                                 break
                             else:
                                 print(
                                     "Wrong choice use  either y to continue or n to stop")
                     elif selected == '2':
                         while True:
-                            print('view your credential below')
+                            print('view your credenttial below')
                             if display_credential():
 
-                                for credential in display_credential():
+                                for creds in display_credential():
                                     print(
                                         f"Account Name:{creds.account_name} Username: {creds.username} Password:{creds.account_password}")
 
@@ -158,7 +199,8 @@ def main():
                                 break
                             else:
                                 print("Enter a valid choice")
-                                elif selected == '5':
+
+                    elif selected == '5':
                         print("Are you sure you want to exit this site yes/no")
 
                         exit == input().lower()
@@ -166,7 +208,6 @@ def main():
                         if exit == 'yes':
                             print("logged out successfully")
                             break
-                        
                         else:
                             if exit == 'no':
                                 continue
@@ -174,11 +215,63 @@ def main():
                     else:
                         print("options not selected")
                         continue
- else:
-                        print("options not selected")
-                        continue
 
-           
+            else:
+                print("The user name does not exist")
+                print("Create Account ")
+                print('*'*40)
+                print('Select Username')
+                username = input()
+                print('\n')
+                print('Password \n')
+                password = input()
+                # save_user(create_user()
+                print("signed up successfully")
+
+                
+        elif option == "no":
+
+            print("Create Account ")
+            print('*'*40)
+            print('Select Username')
+            username = input()
+            print('\n')
+            print('Password \n')
+            password = input()
+
+            verification = user_exist(username)
+            if verification == True:
+                print("The username already exists")
+            else:
+                save_user(create_user(username, password))
+
+            while True:
+                print('*** Login ***')
+                print('Enter username \n')
+                nomname = input()
+                print('\n')
+                print('Enter password')
+                nompass = input()
+
+                if nomname != username or nompass != password:
+                    print("Wrong password or usename")
+
+                    continue
+                else:
+                    print(f"Welcome:{nomname} to your Account ")
+                    print('*'*40)
+
+                    print("Select an option either 1,2,3,4 or 5")
+                    print('\n')
+
+                    while True:
+                        print('1:Add credential')
+                        print('2:View saved credential')
+                        print('3:Delete Credentials')
+                        print('4:Search Credentials')
+                        print('5:Leave')
+                        selected = input()
+
                         if selected == '1':
                             while True:
                                 print('Do you want to proceed? y/n')
@@ -214,7 +307,7 @@ def main():
                                 else:
                                     print(
                                         "Wrong choice use  either y to continue or n to stop")
-                                    elif selected == '2':
+                        elif selected == '2':
                             while True:
                                 print('view your credenttial below')
                                 if display_credential():
@@ -242,6 +335,7 @@ def main():
                         elif selected == '3':
                             while True:
                                 print("search for account to delete credential")
+
                                 find = input()
                                 if existing_credentials(find):
                                     accfind = find_credential(find)
@@ -282,18 +376,15 @@ def main():
                         elif selected == '5':
                             break
                             
-
                         else:
                             print("options not selected")
                             continue
 
         elif option == 'ex':
-            print("Thank you ........")
+            print("BY ........")
             break
         else:
-            print("You entered a wrong option ")
+            print("I really didn't get that")
 
-
-                    
 if __name__ == '__main__':
     main()
